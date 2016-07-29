f72c39816cf4efe4c073ec45af462241e41879ac
Robert Bunch <rob@digitalcrafts.com>
2016-07-29 13:48:18 -0400

    Update code to contain add the flip match

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
+var cards = [
+	'<img src="http://www.relatably.com/m/img/iron-man-meme-generator/mc-iron-man-meme-generator-stop-i-am-iron-man-ce3373.jpg">',
+	'<img src="image2.png">'
+];
+
+
+
 // All code will wait until the DOM is ready!
 $(document).ready(function(){
 	var gridSize = 4;
@@ -6,6 +13,9 @@ $(document).ready(function(){
 
 	var mgHTML = '';
 	for(var i = 0; i < gridSize; i++){
+		if(i < 2){card = cards[0];
+		}else{card = cards[1];
+		}
 		mgHTML += '<div class="mg-tile col-sm-3">';
 			mgHTML += '<div class="mg-tile-inner">';
 				mgHTML += '<div class="mg-front">'+card+'</div>';
@@ -15,5 +25,27 @@ $(document).ready(function(){
 	}
 
 $('.mg-contents').html(mgHTML);
+
+    $(document).ready(function() {
+        $('.mg-tile-inner').click(function() {
+            $(this).toggleClass('flip');
+            var cardsUp = $('.flip');
+            if(cardsUp.length == 2){
+            	//check to see if they are the same!
+            	if(cardsUp.find('img')[0].src == cardsUp.find('img')[1].src){
+            		//The pictures are the same. This is a match!
+            		console.log(cardsUp);
+            		cardsUp.addClass('matched');
+            	}else{
+            		//The user has flipped two cards, and they don't match.
+            		setTimeout(function(){
+            			cardsUp.removeClass('flip');	
+            		}, 1000)
+            		
+            	}
+            }
+
+        });
+    });
 
 });
